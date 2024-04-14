8cbac20b83e2348bbd32140a778da5464af7bf8d
Ubaid2116 <ukubaid74@gmail.com>
2024-04-14 18:53:17 +0500

    Adding BMI calculator project

---
+++ app.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+console.log(chalk.bold.italic.bgRedBright("\n\tWelcome to Muhammad-Ubaid - BMI Calculator\n"));
+import inquirer from "inquirer";
+import chalk from "chalk";
+let answer = await inquirer.prompt([
+    {
+        name: "num1",
+        type: "number",
+        message: "Please enter your weight in kg:",
+    },
+    {
+        name: "num2",
+        type: "number",
+        message: "Please enter your height in meters:",
+    },
+]);
+let weightInKg = answer.num1;
+let heightInMeters = answer.num2;
+let bmi = weightInKg / (heightInMeters * heightInMeters);
+console.log(chalk.bgYellow.bold(`Your BMI is ${bmi}`));
